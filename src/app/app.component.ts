9f72bf76964437e85cfd34fd1e8bfc98eb9dfcbb
Ilias Moreno <iliasete@gmail.com>
2023-04-12 14:15:58 +0200

    add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Component1Component } from './components/component1/component1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, Component1Component],
   template: `
-    <h1>App Component</h1>
+    <ul>
+      <li><a routerLink="/">Home</a></li>
+      <li><a routerLink="/pageA">PageA</a></li>
+      <li><a routerLink="/pageB">PageB</a></li>
+      <li><a routerLink="/pageC">PageC</a></li>
+    </ul>
+
+    <app-component1></app-component1>
+
+    <hr>
+    
     <router-outlet></router-outlet>
   `,
   styles: []
