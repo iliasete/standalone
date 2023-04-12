f0423b91c93bddcc49ef2fb15fb37d954405eacd
Ilias Moreno <iliasete@gmail.com>
2023-04-12 14:15:41 +0200

    prepare routes to lazyload

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,31 @@
 import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter, Routes } from "@angular/router";
 import { AppComponent } from "./app/app.component";
+import { HomeComponent } from "./app/pages/home/home.component";
 
-bootstrapApplication(AppComponent);
+const ROUTES: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'pageA',
+    loadComponent: () => import('./app/pages/page-a/page-a.component').then(m => m.PageAComponent)
+  },
+  {
+    path: 'pageB',
+    loadComponent: () => import('./app/pages/page-b/page-b.component').then(m => m.PageBComponent)
+  },
+  {
+    path: 'pageC',
+    loadComponent: () => import('./app/pages/page-c/page-c.component').then(m => m.PageCComponent)
+  }
+
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(ROUTES)
+  ]
+});
